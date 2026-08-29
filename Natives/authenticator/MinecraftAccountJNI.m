8bf04f5502b2394b449404f2f2078a943ae8cec9
Aiden6652 <13141062013@163.com>
2026-08-29 21:18:34 +0800

    Fix access token retrieval to allow multiple calls
    Updated the access token retrieval function to allow multiple calls and handle nil checks, preventing crashes.
    
    Signed-off-by: Aiden6652 <13141062013@163.com>

--- Natives/authenticator/MinecraftAccountJNI.m
+++ Natives/authenticator/MinecraftAccountJNI.m
@@ -2,15 +2,17 @@
 #include "jni.h"
 
 JNIEXPORT jstring JNICALL Java_net_kdt_pojavlaunch_value_MinecraftAccount_getAccessTokenFromKeychain(JNIEnv *env, jclass clazz, jstring xuid) {
-    // This function should only be called once
-    static BOOL called = NO;
-    if (called) {
-        abort();
+    // 修复：原版实现只允许调用一次（第二次直接 abort() 崩溃），且无 nil 检查。
+    // 现在：允许多次调用；Keychain 读不到或参数为空时返回空字符串，绝不崩溃。
+    if (xuid == NULL) {
+        return (*env)->NewStringUTF(env, "");
     }
-    called = YES;
 
     const char *xuidC = (*env)->GetStringUTFChars(env, xuid, 0);
     NSString *accessToken = [NSClassFromString(@"MicrosoftAuthenticator") tokenDataOfProfile:@(xuidC)][@"accessToken"];
     (*env)->ReleaseStringUTFChars(env, xuid, xuidC);
+    if (accessToken == nil) {
+        return (*env)->NewStringUTF(env, "");
+    }
     return (*env)->NewStringUTF(env, accessToken.UTF8String);
 }
